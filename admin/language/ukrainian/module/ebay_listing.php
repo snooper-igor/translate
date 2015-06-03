2ea322731d710b97ff194a67d9a2e1b6fad9b1cf
igor snooper <igor.rv.ua@gmail.com>
2015-06-03 19:10:29 +0300

    + new files and translate

---
+++ admin/language/ukrainian/module/ebay_listing.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']     = 'eBay Список';
+
+// Text
+$_['text_module']       = 'Модуль';
+$_['text_success']      = 'Успішно: Внесено зміни в модуль eBay!';
+$_['text_edit']        	= 'Редагувати модуль eBay';
+$_['text_list']         = 'Список макетів';
+$_['text_register']     = 'Ви повинні зареєструватися і включити OpenBay Pro для eBay!';
+$_['text_about'] 		= 'Модуль дисплея eBay дозволяє відображати товари з вашого облікового запису eBay безпосередньо на вашому сайті.';
+$_['text_latest']       = 'Останній';
+$_['text_random']       = 'Випадковий';
+
+// Entry
+$_['entry_name']        = 'Назва модуля';
+$_['entry_username']    = 'eBay логін';
+$_['entry_keywords']    = 'Пошукові слова';
+$_['entry_description'] = 'Включити опис';
+$_['entry_limit']       = 'Ліміт';
+$_['entry_length']      = 'Довжина';
+$_['entry_width']       = 'Ширина';
+$_['entry_height']      = 'Висота';
+$_['entry_site']   		= 'eBay';
+$_['entry_sort']   		= 'Сортування';
+$_['entry_status']   	= 'Статус';
+
+// Error
+$_['error_permission'] = 'У Вас немає доступу до зміни модулю eBay!';
+$_['error_name']       = 'Назва модуля не може бути меншою 3 та більшою 64 символів!';
+$_['error_width']      = 'Необхідна ширина!';
+$_['error_height']     = 'Необхідна висота!';
