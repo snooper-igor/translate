23484a88062b6f56f5d54ebae50c671700a3793d
igor snooper <igor.rv.ua@gmail.com>
2015-06-03 19:11:41 +0300

    + translate UA - module Exchange 1C (Обмін данними з 1С

---
+++ admin/language/ukrainian/module/exchange1c.php
@@ -0,0 +1,68 @@
+<?php
+// Heading
+$_['heading_title']	= 'Обмінн данними з 1C v8.x';
+
+// Text
+$_['text_module']		= 'Модули';
+$_['text_success']		= 'Налаштування модуля оновлені!';
+
+$_['text_tab_general']		= 'Основне';
+$_['text_tab_product']		= 'Вивантаження номенклатури';
+$_['text_tab_order']		= 'Обмін замовленнями';
+$_['text_tab_manual']		= 'Ручний імпорт';
+
+$_['text_upload_success']	= 'Імпорт завершено';
+$_['text_upload_error']		= 'Щось пішло не так. Перегляньте Журнал помилок';
+$_['text_max_filesize']		= 'Файл для завантеження не може бути більшим %s Мб';
+
+$_['text_empty']		= 'Налаштувань покищо немає';
+$_['text_homepage']		= 'Домашня сторінка модуля';
+$_['source_code']       = 'Вихідний код на GitHub';
+$_['text_price_default']	= 'Ціни на сайті';
+
+
+// Entry
+$_['entry_username']		= 'Логін:';
+$_['entry_password']		= 'Пароль:';
+$_['entry_status']		= 'Статус:';
+$_['entry_allow_ip']		= 'Дозволені IP адреси:';
+$_['help_allow_ip']		= 'Розділяти переносом рядка. Якщо порожньо, дозволені всі адреси.';
+
+$_['entry_config_price_type']		= 'Тип ціни вигрузки:';
+$_['entry_customer_group']	= 'Группа покупців:';
+$_['entry_quantity']		= 'Кількість:';
+$_['entry_priority']		= 'Приорітет:';
+$_['entry_action']		= 'Дія';
+$_['entry_flush_product']	= 'Скидувати товари:';
+$_['entry_flush_category']	= 'Скидувати категорії:';
+$_['entry_flush_manufacturer']	= 'Скидувати виробників:';
+$_['entry_flush_quantity']	= 'Скидувати кількість:';
+$_['entry_flush_attribute']	= 'Скидувати атрибути:';
+$_['entry_fill_parent_cats']	= 'Заповнювати батьківські категорії:';
+$_['entry_seo_url']		= 'Генерувати SEO URL: <br> (у зв\'язці <a href="http://opencartforum.ru/files/file/332-deadcow-seo-v21-automod/" target="_blank">Deadcow SEO</a>)';
+$_['entry_seo_url_deadcow']		= 'через Deadcow SEO';
+$_['entry_seo_url_translit']	= 'Транслітерація';
+
+$_['entry_full_log']		= 'Включити лог помилок';
+$_['entry_apply_watermark']	= 'Накладати водяні знаки на зображення';
+$_['text_image_manager'] = 'Менеджер зображень';
+$_['text_browse']        = 'Огляд';
+$_['text_clear']         = 'Очистити';
+
+$_['entry_order_status_to_exchange'] 	= 'Вивантажувати замовлення з статусом:';
+$_['entry_order_status_to_exchange_not'] 	= "- не використовувати -";
+$_['entry_relatedoptions']	= 'Завантажувати характеристики, як пов\'язані опції (потрібен модуль <a href="http://opencartforum.ru/files/file/1501-связанные-опции/">Пов\'язані опції</a>):';
+$_['entry_relatedoptions_help']	= 'в налаштуваннях пов\'язаних опцій обов\'язково включені опції: "Перерахувати кількість", "Оновлювати опції", "Використовувати різні варіанти зв\'язаних опцій" ';
+$_['entry_dont_use_artsync'] = 'Не шукати товари по артикулю:';
+
+$_['entry_order_status']	= 'Статус вивантажених замовлень:';
+$_['entry_order_notify']	= 'Повідомляти користувачів про зміну статусу:';
+$_['entry_order_currency']	= 'Валюта замовлення:';
+
+$_['entry_upload']		= 'Вибрати файл:';
+$_['button_upload']		= 'Завантажити';
+$_['button_insert']		= 'Додати';
+$_['text_confirm']		= 'Підтвердити дію';
+
+// Error
+$_['error_permission']		= 'Доступ заборонено. У вас немає прав управління цим модулем!';
